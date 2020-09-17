6dbe3909a188e9f9c197faff4568df212f231416
Relax <pirr2004@mail.ru>
2020-09-17 07:21:04 +0400

    Задание 2

--- src/main/java/PhonebookApp.java
+++ src/main/java/PhonebookApp.java
@@ -4,63 +4,85 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class PhonebookApp {
+    private static Hashtable phonebookDB = new Hashtable();
+    private static Pattern patternPhoneNumber = Pattern.compile("\\d\\d-\\d\\d-\\d\\d");
+    private static Pattern patternName = Pattern.compile("[a-zA-Z][a-zA-Z0-9]{0,14}");
+
     public static void main(String[] args) {
-        Hashtable phonebook = new Hashtable();
         Scanner scanner = new Scanner(System.in);
-        Pattern patternPhoneNumber = Pattern.compile("\\d\\d-\\d\\d-\\d\\d");
-        Pattern patternName = Pattern.compile("[a-zA-Z][a-zA-Z0-9]{0,14}");
 
         System.out.println("Enter command [add, find, del, list, exit]:");
 
         while (true) {
             String command = scanner.nextLine();
             String[] commandWords = command.split(" ");
-            switch (commandWords[0]) {
-                case "add":
-                    if (patternPhoneNumber.matcher(commandWords[1]).matches()) {
-                        if (patternName.matcher(commandWords[2]).matches()) {
-                            phonebook.put(commandWords[1], commandWords[2]);
-                            System.out.println("Added phone number: " + commandWords[1] + " Name: " + commandWords[2]);
-                        } else {
-                            System.out.println("Name is not valid");
-                        }
-                    } else {
-                        System.out.println("Phone number is not valid.");
-                    }
-                    break;
-                case "find":
-                    if (patternPhoneNumber.matcher(commandWords[1]).matches()) {
-                        if (phonebook.get(commandWords[1]) == null) {
-                            System.out.println("Phone number not found.");
-                        } else {
-                            System.out.println(phonebook.get(commandWords[1]));
-                        }
-                    } else {
-                        System.out.println("Phone number is not valid.");
-                    }
-                    break;
-                case "del":
-                    if (patternPhoneNumber.matcher(commandWords[1]).matches()) {
-                        phonebook.remove(commandWords[1]);
-                        System.out.println("Phone number deleted");
-                    } else {
-                        System.out.println("Phone number is not valid.");
-                    }
-                    break;
-                case "list":
-                    Enumeration phoneNumbers = phonebook.keys();
-                    String record;
-                    while (phoneNumbers.hasMoreElements()) {
-                        record = (String) phoneNumbers.nextElement();
-                        System.out.println(record + " " + phonebook.get(record));
-                    }
-                    break;
-                case "exit":
-                    return;
-                default:
-                    System.out.println("Entered command wrong!");
-                    break;
+            if (phonebook(commandWords)) return;
+        }
+    }
+
+    private static boolean phonebook(String[] commandWords) {
+        switch (commandWords[0]) {
+            case "add":
+                add(commandWords);
+                break;
+            case "find":
+                find(commandWords);
+                break;
+            case "del":
+                del(commandWords);
+                break;
+            case "list":
+                list();
+                break;
+            case "exit":
+                return true;
+            default:
+                System.out.println("Entered command wrong!");
+                break;
+        }
+        return false;
+    }
+
+    private static void list() {
+        Enumeration phoneNumbers = phonebookDB.keys();
+        String record;
+        while (phoneNumbers.hasMoreElements()) {
+            record = (String) phoneNumbers.nextElement();
+            System.out.println(record + " " + phonebookDB.get(record));
+        }
+    }
+
+    private static void del(String[] commandWords) {
+        if (patternPhoneNumber.matcher(commandWords[1]).matches()) {
+            phonebookDB.remove(commandWords[1]);
+            System.out.println("Phone number deleted");
+        } else {
+            System.out.println("Phone number is not valid.");
+        }
+    }
+
+    private static void find(String[] commandWords) {
+        if (patternPhoneNumber.matcher(commandWords[1]).matches()) {
+            if (phonebookDB.get(commandWords[1]) == null) {
+                System.out.println("Phone number not found.");
+            } else {
+                System.out.println(phonebookDB.get(commandWords[1]));
             }
+        } else {
+            System.out.println("Phone number is not valid.");
+        }
+    }
+
+    private static void add(String[] commandWords) {
+        if (patternPhoneNumber.matcher(commandWords[1]).matches()) {
+            if (patternName.matcher(commandWords[2]).matches()) {
+                phonebookDB.put(commandWords[1], commandWords[2]);
+                System.out.println("Added phone number: " + commandWords[1] + " Name: " + commandWords[2]);
+            } else {
+                System.out.println("Name is not valid");
+            }
+        } else {
+            System.out.println("Phone number is not valid.");
         }
     }
 }
